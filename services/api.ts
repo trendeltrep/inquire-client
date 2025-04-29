4df3841e18c057de5e6113e2de3b04502b5fa1b6
trendeltrep <maxim.krikukha20031@gmail.com>
2025-04-29 23:24:29 +0300

    fix: domain issue

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'inquire-server-production.up.railway.app/api',
+  baseURL: 'https://inquire-server-production.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
