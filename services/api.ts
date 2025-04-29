8844ed6e205a067923eb96344c3a90b0a6acda64
trendeltrep <maxim.krikukha20031@gmail.com>
2025-04-29 22:50:44 +0300

    feat: added domain

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'inquire-server-production.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
